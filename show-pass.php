5d2b6e020d7e909324911c92c0a85fc3c5752f41
LucaBrivio183 <l.brivio4@gmail.com>
2023-04-11 17:23:52 +0200

    feat: minor style changes

--- show-pass.php
+++ show-pass.php
@@ -11,14 +11,18 @@ $_SESSION['password-length'] = $_GET['password-length'];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootsrap cdn -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>PHP Strong Password Generator</title>
 </head>
 
 <body class="vh-100 d-flex flex-column justify-content-center bg-primary-subtle">
-    <div class="container">
-        <h1 class="p-3 text-center">Your password is:</h1>
-        <h2 class="p-3 text-center"><?php echo randomPassword($_GET['password-length']) ?></h2>
+    <div class="container text-center">
+        <h1 class="p-3">Your password is:</h1>
+        <h2 class="p-3"><?php echo randomPassword($_SESSION['password-length']) ?></h2>
     </div>
+    <!-- bootstrap script -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 
 </html>
