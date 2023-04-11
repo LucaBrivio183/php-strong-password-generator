9ff879ca62a0e3d4a7ca47986481535fcc9a893a
LucaBrivio183 <l.brivio4@gmail.com>
2023-04-11 17:04:33 +0200

    feat: milestone 3

--- show-pass.php
+++ show-pass.php
@@ -1,6 +1,7 @@
 <?php
 include __DIR__ . '/session.php';
 include __DIR__ . '/functions.php';
+$_SESSION['password-length'] = $_GET['password-length'];
 ?>
 
 <!DOCTYPE html>
@@ -16,7 +17,7 @@ include __DIR__ . '/functions.php';
 <body class="vh-100 d-flex flex-column justify-content-center bg-primary-subtle">
     <div class="container">
         <h1 class="p-3 text-center">Your password is:</h1>
-        <h2 class="p-3 text-center"><?php echo randomPassword($_SESSION['password-length']) ?></h2>
+        <h2 class="p-3 text-center"><?php echo randomPassword($_GET['password-length']) ?></h2>
     </div>
 </body>
 
